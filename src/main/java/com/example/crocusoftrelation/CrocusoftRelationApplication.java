a8b1a827117bbdabcb8b2b1f5cab1773c2a5109f
Abbas <abbas99musayev@gmail.com>
2021-10-27 10:22:24 +0400

    initial commit

---
+++ src/main/java/com/example/crocusoftrelation/CrocusoftRelationApplication.java
@@ -0,0 +1,13 @@
+package com.example.crocusoftrelation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrocusoftRelationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrocusoftRelationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crocusoftrelation/CrocusoftRelationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crocusoftrelation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrocusoftRelationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
